9c4d76f66babb7583b03f88d08c370a573e45e4b
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-06-17 19:39:40 +0530

    added /collections endpoint to fetch all collection names

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from server.database.client import get_client
 
 app = FastAPI()
 
@@ -6,3 +7,12 @@ app = FastAPI()
 def main():
     """Homepage"""
     return {"Hello from": "server"}
+
+
+@app.get("/collections")
+def get_collections(client=Depends(get_client)):
+    collections = client.list_collections()
+    print("Collections:", collections)
+    collection_names = [getattr(c, "name", str(c)) for c in collections]
+
+    return {"collections": collection_names}
