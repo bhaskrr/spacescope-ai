788e6007335d64abe0dbcf46a4652f1623de5782
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-06-20 23:57:14 +0530

    added retriever.py

---
+++ server/database/retriever.py
@@ -0,0 +1,27 @@
+from chromadb.utils.embedding_functions import DefaultEmbeddingFunction
+from server.database.client import get_client
+
+default_ef = DefaultEmbeddingFunction()
+
+
+def retrieve_relevant_documents(query: str) -> dict:
+    """Retrieves relevant documents from the vector database
+    
+    Args:
+        query (str): The input query
+    
+    Returns:
+        relevant_docs (dict): The retrieved docs
+    
+    """
+    # Initialize the vector database client
+    client = get_client()
+
+    # Get the collection
+    collection = client.get_collection(name="space_docs", embedding_function=default_ef)
+
+    # Query the collection
+    relevant_docs = collection.query(query_texts=query, n_results=3)
+
+    # Return the retrieved documents
+    return relevant_docs
