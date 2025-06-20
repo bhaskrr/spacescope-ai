26c29b640e7828c394066108b47ccd12e59594c8
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-06-20 23:52:58 +0530

    added enum input to inputquery schema

--- server/schemas/route_schemas.py
+++ server/schemas/route_schemas.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
+from enum import Enum
+
+
+class ModeEnum(Enum):
+    normal = "normal"
+    rag = "rag"
 
 
 class InputQuery(BaseModel):
     query: str
+    mode: ModeEnum
