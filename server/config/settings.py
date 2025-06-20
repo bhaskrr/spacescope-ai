c90483b028c00d818a09df2731b723920d926f86
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-06-20 09:52:53 +0530

    added cors related variables

--- server/config/settings.py
+++ server/config/settings.py
@@ -1,5 +1,5 @@
 import os
-from typing import LiteralString
+from typing import LiteralString, List
 from dotenv import find_dotenv, load_dotenv
 
 # Load environment variables
@@ -10,3 +10,6 @@ class AppSettings:
     CHROMA_HOST: LiteralString = os.environ.get("CHROMA_HOST")
     CHROMA_PORT: int = os.environ.get("CHROMA_PORT")
     CHROMA_TOKEN: LiteralString = os.environ.get("CHROMA_TOKEN")
+    CORS_ORIGINS: List[str] = os.environ.get("CORS_ORIGINS")
+    CORS_METHODS: List[str] = os.environ.get("CORS_METHODS")
+    CORS_HEADERS: List[str] = os.environ.get("CORS_HEADERS")
